5b2b54a189dc064b647e200db90a198d369c8979
Shobhit Rajpurohit <shobhit.21it465@rtu.ac.in>
2025-04-25 17:26:16 +0530

    Initial FastAPI backend commit

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:shobhit13@localhost/medical_db"
+load_dotenv()  # Load from .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
 def get_db():
